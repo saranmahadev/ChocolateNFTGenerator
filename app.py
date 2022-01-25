7d29d5dcc84998b3b90d499503501793dc0ef1d7
Saran Mahadev <sarandevnet@gmail.com>
2022-01-25 15:53:39 +0530

    Added Managing Area

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask,render_template,request,jsonify
+from manage import manage
 from nft import Creator
 
 app = Flask(__name__,template_folder='templates')
+app.register_blueprint(manage)
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
 @app.route('/mint',methods=['POST'])
-def mint():    
-    with Creator() as creator:        
-        return jsonify(creator.mint_nft(int(request.json["count"])))
+def mint():              
+    with Creator() as c:        
+        return jsonify(c.mint_nft(int(request.json["count"])))
 
 if __name__ == '__main__':
     app.run(debug=True)
